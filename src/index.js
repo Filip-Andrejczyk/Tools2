225d73899b5291ce23ddc40ea755aae7f1470c49
Filip-Andrejczyk <filip.andrejczyk@student.wat.edu.pl>
2021-08-03 15:19:01 +0200

    dzialawyskakiwanie_i_zmiana

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import html from "./index.html"; //potrzebne to?
 //import './style.css';
 import Ksiek from './assets/pan-krzysztof.jpg';
 import Major from './assets/pan-major.jpg';
+import siema from './assets/siema.jpg';
 
 
 function component()
@@ -18,21 +19,47 @@ function component()
     return element;
 }
 
+var liczniczek = 0;
 $(function () {
   function zamienobrazki() {
-    if($('#ksiek').prop('src') == Ksiek){
-      $('#ksiek').attr('src', Major);
-      $('#major').attr('src', Ksiek);
-    }
-    else{
-      $('#ksiek').attr('src', Ksiek);
-      $('#major').attr('src', Major);
-    }
+      liczniczek++;
+
+      if($('#ksiek').prop('src') == Ksiek)
+      {
+        if($('#theImg').prop('src') == siema){
+          if($("#tlo").hasClass("obrazki--hide")){
+            $('#tlo').removeClass("obrazki--hide");
+            $('#tlo').addClass("obrazki--hidesiema");
+          }
+        }
+        $('#ksiek').attr('src', Major);
+        $('#major').attr('src', Ksiek);
+        $('.licznik').html('Licznik Borzy: '+ liczniczek);
+      }
+      else if($('#ksiek').prop('src') == Major)
+      {
+        if($('#theImg').prop('src') == siema){
+          if($("#tlo").hasClass("obrazki--hide")){
+            $('#tlo').removeClass("obrazki--hide");
+            $('#tlo').addClass("obrazki--hidesiema");
+          }
+        }
+        $('#ksiek').attr('src', Ksiek);
+        $('#major').attr('src', Major);
+        $('.licznik').html('Licznik Borzy: '+ liczniczek);
+      }
+      if(liczniczek == 3){
+        $('.licznik').html('iiiiiii??');
+        $('#tlo').prepend($('<img>',{id:'theImg',src: siema}))
+        $('#tlo').addClass("obrazki--hide");
+      }
+      else if(liczniczek == 7){
+        $('.licznik').html('iiiiiii??');
+        $('#tlo').addClass("obrazki--hide");
+        $('#tlo').removeClass("obrazki--hidesiema");
+      }
   }
   $("#przycisk").on("click", zamienobrazki);
 });
 
-
-
-  
 document.body.appendChild(component());
